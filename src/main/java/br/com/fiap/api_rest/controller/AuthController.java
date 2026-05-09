ad5b13f3333578c9520731c18b0a62fbc4559dc5
laura853 <lauralopess2006@gmail.com>
2026-05-08 22:47:23 -0300

    criando o securityFilter para validar as credenciais do usuário

--- src/main/java/br/com/fiap/api_rest/controller/AuthController.java
+++ src/main/java/br/com/fiap/api_rest/controller/AuthController.java
@@ -1,10 +1,13 @@
 package br.com.fiap.api_rest.controller;
 
 import br.com.fiap.api_rest.dto.AuthDTO;
+import br.com.fiap.api_rest.dto.LoginResponseDTO;
 import br.com.fiap.api_rest.dto.RegisterDTO;
 import br.com.fiap.api_rest.model.Usuario;
 import br.com.fiap.api_rest.repository.UsuarioRepository;
+import br.com.fiap.api_rest.service.TokenService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -18,7 +21,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
     private final UsuarioRepository usuarioRepository;
+    @Autowired
     private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
 
     public AuthController(UsuarioRepository usuarioRepository){
         this.usuarioRepository = usuarioRepository;
@@ -32,7 +39,10 @@ public class AuthController {
 
         //Autentica esse token
         var auth = this.authenticationManager.authenticate(usuarioSenha);
-        return ResponseEntity.ok().build();
+
+        var token = tokenService.generateToken((Usuario) auth.getPrincipal());
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+
 
     }
 
--- src/main/java/br/com/fiap/api_rest/security/SecurityConfigurations.java
+++ src/main/java/br/com/fiap/api_rest/security/SecurityConfigurations.java
@@ -1,6 +1,7 @@
 package br.com.fiap.api_rest.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,11 +13,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
-
+    @Autowired
+    private  SecurityFilter securityFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
         return httpSecurity
@@ -25,11 +28,12 @@ public class SecurityConfigurations {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests( auth ->
                         auth
-                                .requestMatchers(HttpMethod.POST, "/produtos").hasRole("ADMIN")
-                                .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/produtos").hasRole("ADMIN")
                                 .anyRequest().authenticated()
                 )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
